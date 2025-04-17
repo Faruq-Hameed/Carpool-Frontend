fabbc73ccb4e264e861ad06ef94e83fc294fabc2
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-04-17 15:27:57 +0100

    updated pacages

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './navigation/AppNavigator';
-
+import AppNavigator from './src/navigation/AppNavigator';
 // Root component for the app
-export default function App() {
-  return (
-    // Wrap the navigation container to manage navigation state
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+export default function App() {return (// Wrap the navigation container to manage navigation state
+<NavigationContainer><AppNavigator /></NavigationContainer>
   );
 }
