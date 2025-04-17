346f8289015e8d176922cdf655a6e7f888522ede
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-04-17 15:34:36 +0100

    inititated migration to ts from js

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
-// Root component for the app
-export default function App() {return (// Wrap the navigation container to manage navigation state
-<NavigationContainer><AppNavigator /></NavigationContainer>
-  );
-}
